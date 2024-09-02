9f1522e2239992de075cfac61420efb99200a4ab
Vaishnavi <vaishnavicode3@gmail.com>
2024-09-02 20:54:33 +0530

    Added basic navbar in Menu and a description in About

--- blackjack-app/src/components/About.tsx
+++ blackjack-app/src/components/About.tsx
@@ -1,7 +1,8 @@
 function About() {
     return (
         <>
-            <h1>hehehe</h1>
+            <h1>About Us</h1>
+            <p>I created this to understand the fundamentals of react and make something me and my friends could use. In making this project I realized I am very bad at designing and tackling all aspects of website design. So this is my redemption arc.</p>
         </>
     );
 }
